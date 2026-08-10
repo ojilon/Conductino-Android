fade4077cb5683382b4bd0cbd75ba1568a5b018f
OJILONG YUSUF <89400192+ojilon@users.noreply.github.com>
2026-08-10 11:22:42 +0300

    Init DownloadStore on app start

--- app/src/main/java/com/conductino/study/ConductinoApplication.java
+++ app/src/main/java/com/conductino/study/ConductinoApplication.java
@@ -3,13 +3,12 @@ package com.conductino.study;
 import android.app.Application;
 
 import com.conductino.study.core.NativeCore;
+import com.conductino.study.downloads.DownloadStore;
 import com.conductino.study.logging.LogManager;
 import com.conductino.study.settings.SettingsManager;
 
-import android.util.Log;
-
 /**
- * Process-wide singleton. Loads the C backend, boots settings + logging.
+ * Process-wide singleton. Loads settings, downloads store, native core.
  */
 public class ConductinoApplication extends Application {
 
@@ -23,10 +22,9 @@ public class ConductinoApplication extends Application {
         LogManager.initializeFileLogging(this);
         LogManager.i("App", "Conductino Study starting up");
 
-        // Load settings/*.json before anything else touches config.
         SettingsManager.get().load(this);
+        DownloadStore.get().init(this);
 
-        // Bring the native (C) core online through JNI.
         NativeCore.get().boot(getFilesDir().getAbsolutePath());
     }
 
